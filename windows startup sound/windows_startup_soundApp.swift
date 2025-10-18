861e637678e44618dc4b50bea5465849bc692663
aKi-1201 <b10501015@ntu.edu.tw>
2025-10-18 18:10:18 +0800

    Initial Commit

---
+++ windows startup sound/windows_startup_soundApp.swift
@@ -0,0 +1,17 @@
+//
+//  windows_startup_soundApp.swift
+//  windows startup sound
+//
+//  Created by 114-1iosClassStudent05 on 2025/10/18.
+//
+
+import SwiftUI
+
+@main
+struct windows_startup_soundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
